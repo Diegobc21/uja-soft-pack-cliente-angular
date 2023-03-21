2b77825b56d06e59786460bbfe40e73cb7d8d06a
Diegobc21 <dbc00009@red.ujaen.es>
2023-03-21 15:39:14 +0100

    Cambios menores

--- src/app/core/interfaces/extras.interface.ts
+++ src/app/core/interfaces/extras.interface.ts
@@ -1,4 +1,4 @@
 export interface Extras {
-  titulo: string;
-  fuente: string;
+  titulo: string,
+  fuente: string
 }
--- src/app/core/interfaces/software.interface.ts
+++ src/app/core/interfaces/software.interface.ts
@@ -2,11 +2,11 @@ import {Category} from "./category.interface";
 import {Extras} from "./extras.interface";
 
 export interface Software {
-  nombre: string;
-  descripcion: string;
-  categorias: Category;
-  descarga: string;
-  winget_id: string;
-  extras?: Extras[];
-  iconURL: string;
+  nombre: string,
+  descripcion: string,
+  categorias: Category,
+  descarga: string,
+  winget_id: string,
+  extras?: Extras[],
+  iconURL: string,
 }
